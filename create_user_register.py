dbb436574621deb9dd2c5f3e99a8250e4b8bbf6d
Venkatakrishnan-opencv <venkat.1796@gmail.com>
2017-11-11 20:00:04 +0530

    Update create_user_register.py

--- create_user_register.py
+++ create_user_register.py
@@ -4,9 +4,9 @@ print("Opened database successfully")
 
 #conn.execute("drop table user_details")
 conn.execute('CREATE TABLE user_details(user_id VARCHAR(50) primary key,\
-	password VARCHAR(20),FirstName VARCHAR(80) not null,LastName VARCHAR(20),\
-	email VARCHAR(100) not null,DateOfBirth CHAR(10) not null,\
-	Street varchar(100) not null,\
+	password VARCHAR(20),firstName VARCHAR(80) not null,lastName VARCHAR(20),\
+	email VARCHAR(100) not null,dateofbirth CHAR(10) not null,\
+	street varchar(100) not null,\
 	city varchar(20) not null,pincode integer(6) not null,mobileNo integer(10) not null,\
 	walletBalance integer(4) not null)')
 
