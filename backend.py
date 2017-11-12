1f8e90c55c3a964b4ad8c2ad5afbed32f81a3bb2
Vishalkks <vishalkks@gmail.com>
2017-11-13 00:50:04 +0530

    qr code functionality added

--- backend.py
+++ backend.py
@@ -1,7 +1,7 @@
 from flask import Flask, render_template, request
 import pyqrcode
 import qrtools
-import glob
+import glob,os
 import sqlite3
 app = Flask(__name__)
 
@@ -15,7 +15,18 @@ def login():
 	
 @app.route('/register')
 def register():
-	return render_template('register.html')
+	userId = request.form['user_id']
+	fname = request.form['firstName']
+	lname = request.form['lastName']
+	email = request.form['email']
+	password = request.form['password']
+	hash_password = generate_password_hash(password)
+	if fname and email and password:
+		cursor.execute("INSERT INTO user_details (fname,lname,email,password) VALUES ('"+fname+"','"+lname+"','"+email+"','"+hash_password+"');")
+		conn.commit()
+		return redirect(url_for('login'),code=303)
+	else:
+		return json.dumps({'html':'<span>Enter the required fields</span>'})
 	
 @app.route('/train_details')
 def train_details():
@@ -35,6 +46,7 @@ def train_table():
 		cur = con.cursor()
 		cur.execute("select * from train where source = \'"+src+"\' and destination = \'" +dst+"\'")
 		rows = cur.fetchall()
+		print(rows)
 		return render_template('book_tickets.html', rows1=rows, source = src, destination = dst)
 		
 @app.route('/enterquantity', methods = ['POST', 'GET'])
@@ -70,24 +82,30 @@ def generate_qrcode():
 		src = request.form['src']
 		dst = request.form['dst']
 		timings = request.form['timings']
-		qrcode = pyqrcode.create(data)
-		latestBooking = max(map(lambda x : int(x.split('.')[0]), glob.glob('/qrcodes/'+username+'/')))
+		qrcode = pyqrcode.create(src+","+dst+","+timings)
+		#latestBooking = max(map(lambda x : int(x.split('.')[0]), glob.glob('/qrcodes/'+username+'/*')))
+		latestBooking = map(lambda x : int(x.split('.')[0][8:]), glob.glob('qrcodes'+'/*'))
 		if(latestBooking):
-			latestBooking+=1
+			latestBooking = max(latestBooking) + 1
 		else:
 			latestBooking = 1
-		fname = '/qrcodes/'+username+'/'+latestBooking+".png"
+		#fname = '/qrcodes/'+username+'/'+latestBooking+".png"
+		os.chdir('qrcodes')
+		fname = str(latestBooking)+".png"
 		with open(fname,'w+') as fstream:
 			qrcode.png(fstream, scale=5)
+		os.chdir("../")
 		
-	return fname+".png"
+	return fname
 	
-@app.route('/readqr',methods = ['GET'])
-def read_qrcode(method,qrcode=0):
-	#0 for file, 1 for webcam, qrcode must be a file if method is 0
+@app.route('/readqr',methods = ['POST'])
+def read_qrcode():
+	#0 xfor file, 1 for webcam, qrcode must be a file if method is 0
 	qr = qrtools.QR()
-	if method == 0:
-		qr.decode(qrcode)
+	qrcode = request.form['qrcode']
+	method = request.form['method']
+	if int(method) == 0:
+		qr.decode(open('qrcodes/'+qrcode,'r'))
 	else:
 		qr.decode_webcam()
 		
--- db/create_user_register.py
+++ db/create_user_register.py
@@ -5,7 +5,7 @@ print("Opened database successfully")
 #conn.execute("drop table user_details")
 conn.execute('CREATE TABLE user_details(user_id VARCHAR(50) primary key,\
 	firstName VARCHAR(80) not null,lastName VARCHAR(20),email VARCHAR(100) not null,\
-	password VARCHAR(20) not null,dateofbirth CHAR(10) not null,\
+	password VARCHAR(100) not null,dateofbirth CHAR(10) not null,\
 	street varchar(100) not null,\
 	city varchar(20) not null,pincode integer(6) not null,mobileNo integer(10) not null,\
 	walletBalance integer(5) not null)')
