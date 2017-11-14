29552ead9bdd754b78648790cd3e715fb20e833d
SrilakshmiBharadwaj <srilakshmi.baradwaj@gmail.com>
2017-11-14 21:12:20 +0530

    register_page_integration

--- create_user_register.py
+++ create_user_register.py
@@ -2,7 +2,7 @@ import sqlite3
 conn = sqlite3.connect('Emetro.db')
 print("Opened database successfully")
 
-conn.execute("drop table user_details")
+#conn.execute("drop table user_details")
 conn.execute('CREATE TABLE user_details(user_id VARCHAR(50) primary key,\
 	firstName VARCHAR(80) not null,lastName VARCHAR(20),email VARCHAR(100) not null,\
 	password VARCHAR(100) not null,dateofbirth CHAR(10) not null,\
